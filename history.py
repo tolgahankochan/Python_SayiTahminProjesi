cdaa1992047e5821352e2d89c66a562acaeb3250
Tolga <tolgahankcn61@gmail.com>
2022-07-12 09:15:17 +0300

    Initial commit

---
+++ history.py
@@ -0,0 +1,107 @@
+    sayi5=1
+if sayi5 == "B":
+    sayi5=2    
+if sayi5 == "C":
+    sayi5=4   
+if sayi5 == "D":
+    sayi5=8
+if sayi5 == "E":
+    sayi5=16 
+if sayi5 == "X":
+    sayi5=0  
+
+print ("Aklından tuttuğun sayı :",int((sayi1)+(sayi2)+(sayi3)+(sayi4)+(sayi5)))
+%clear
+print ("Doğru mu bildim ",ad)
+runfile('C:/Users/tolga/Desktop/TK/PYHTON/bilmece.py', wdir='C:/Users/tolga/Desktop/TK/PYHTON')
+%clear
+print ("Doğru mu bildim ",ad,"?")
+print ("Aklından tuttuğun sayı :",int((sayi1)+(sayi2)+(sayi3)+(sayi4)+(sayi5)),ad)
+%clear
+runfile('C:/Users/tolga/Desktop/TK/PYHTON/bilmece.py', wdir='C:/Users/tolga/Desktop/TK/PYHTON')
+%clear
+runfile('C:/Users/tolga/Desktop/TK/PYHTON/bilmece.py', wdir='C:/Users/tolga/Desktop/TK/PYHTON')
+%clear
+runfile('C:/Users/tolga/Desktop/TK/PYHTON/bilmece.py', wdir='C:/Users/tolga/Desktop/TK/PYHTON')
+%clear
+runfile('C:/Users/tolga/Desktop/TK/PYHTON/bilmece.py', wdir='C:/Users/tolga/Desktop/TK/PYHTON')
+%clear
+runfile('C:/Users/tolga/Desktop/TK/PYHTON/bilmece.py', wdir='C:/Users/tolga/Desktop/TK/PYHTON')
+%clear
+runfile('C:/Users/tolga/Desktop/TK/PYHTON/bilmece.py', wdir='C:/Users/tolga/Desktop/TK/PYHTON')
+%clear
+runfile('C:/Users/tolga/Desktop/TK/PYHTON/bilmece.py', wdir='C:/Users/tolga/Desktop/TK/PYHTON')
+
+## ---(Wed Oct 28 12:17:19 2020)---
+runfile('C:/Users/tolga/Desktop/TK/PYHTON/bilmece.py', wdir='C:/Users/tolga/Desktop/TK/PYHTON')
+%clear
+runfile('C:/Users/tolga/Desktop/TK/PYHTON/dolarMatik.py', wdir='C:/Users/tolga/Desktop/TK/PYHTON')
+
+## ---(Thu Oct 29 19:49:46 2020)---
+runfile('C:/Users/tolga/Desktop/TK/PYHTON/alarm.py', wdir='C:/Users/tolga/Desktop/TK/PYHTON')
+%clear
+runfile('C:/Users/tolga/Desktop/TK/PYHTON/alarm.py', wdir='C:/Users/tolga/Desktop/TK/PYHTON')
+%clear
+runfile('C:/Users/tolga/Desktop/TK/PYHTON/telefonnmbr.py', wdir='C:/Users/tolga/Desktop/TK/PYHTON')
+%clear
+
+## ---(Fri Oct 30 22:18:11 2020)---
+runfile('C:/Users/tolga/Desktop/TK/PYHTON/telefonnmbr.py', wdir='C:/Users/tolga/Desktop/TK/PYHTON')
+
+## ---(Tue Nov  3 18:59:42 2020)---
+runfile('C:/Users/tolga/Desktop/TK/PYHTON/telefonnmbr.py', wdir='C:/Users/tolga/Desktop/TK/PYHTON')
+%clear
+runfile('C:/Users/tolga/Desktop/TK/PYHTON/telefonnmbr.py', wdir='C:/Users/tolga/Desktop/TK/PYHTON')
+%clear
+runfile('C:/Users/tolga/Desktop/TK/PYHTON/telefonnmbr.py', wdir='C:/Users/tolga/Desktop/TK/PYHTON')
+
+## ---(Fri Nov 20 18:46:32 2020)---
+runfile('C:/Users/tolga/Desktop/TK/PYHTON/bilmece.py', wdir='C:/Users/tolga/Desktop/TK/PYHTON')
+%clear
+runfile('C:/Users/tolga/Desktop/TK/PYHTON/dolarMatik.py', wdir='C:/Users/tolga/Desktop/TK/PYHTON')
+
+## ---(Fri Dec 18 00:04:33 2020)---
+runfile('C:/Users/tolga/Desktop/TK/PYHTON/bilmece.py', wdir='C:/Users/tolga/Desktop/TK/PYHTON')
+
+## ---(Mon Dec 28 20:06:26 2020)---
+runfile('C:/Users/tolga/Desktop/TK/PYHTON/bilmece.py', wdir='C:/Users/tolga/Desktop/TK/PYHTON')
+runfile('C:/Users/tolga/Desktop/TK/PYHTON/bilmece.py', wdir='C:/Users/tolga/Desktop/TK/PYHTON')
+runfile('C:/Users/tolga/Desktop/TK/PYHTON/dolarMatik.py', wdir='C:/Users/tolga/Desktop/TK/PYHTON')
+runfile('C:/Users/tolga/Desktop/TK/PYHTON/dolarMatik.py', wdir='C:/Users/tolga/Desktop/TK/PYHTON')
+
+## ---(Thu Dec 31 20:10:02 2020)---
+runfile('C:/Users/tolga/Desktop/TK/PYHTON/bilmece.py', wdir='C:/Users/tolga/Desktop/TK/PYHTON')
+runfile('C:/Users/tolga/Desktop/TK/PYHTON/bilmece.py', wdir='C:/Users/tolga/Desktop/TK/PYHTON')
+runfile('C:/Users/tolga/Desktop/TK/PYHTON/bilmece.py', wdir='C:/Users/tolga/Desktop/TK/PYHTON')
+
+## ---(Mon Jan 11 14:11:43 2021)---
+runfile('C:/Users/tolga/Desktop/TK/PYHTON/bilmece.py', wdir='C:/Users/tolga/Desktop/TK/PYHTON')
+
+## ---(Sat Feb  6 10:54:54 2021)---
+runfile('C:/Users/tolga/Desktop/TK/PYHTON/bilmece.py', wdir='C:/Users/tolga/Desktop/TK/PYHTON')
+runfile('C:/Users/tolga/Desktop/TK/PYHTON/dolarMatik.py', wdir='C:/Users/tolga/Desktop/TK/PYHTON')
+
+## ---(Sun Feb  7 12:05:56 2021)---
+runfile('C:/Users/tolga/Desktop/TK/PYHTON/bilmece.py', wdir='C:/Users/tolga/Desktop/TK/PYHTON')
+
+## ---(Mon Feb 22 22:53:32 2021)---
+runfile('C:/Users/tolga/Desktop/TK/PYHTON/bilmece.py', wdir='C:/Users/tolga/Desktop/TK/PYHTON')
+runfile('C:/Users/tolga/Desktop/TK/PYHTON/dolarMatik.py', wdir='C:/Users/tolga/Desktop/TK/PYHTON')
+
+## ---(Fri Jun 11 17:32:59 2021)---
+runfile('C:/Users/tolga/Desktop/TK/PYHTON/bilmece.py', wdir='C:/Users/tolga/Desktop/TK/PYHTON')
+
+## ---(Sun Jun 20 13:48:54 2021)---
+runfile('C:/Users/tolga/Desktop/TK/PYHTON/bilmece.py', wdir='C:/Users/tolga/Desktop/TK/PYHTON')
+
+## ---(Thu Oct  7 11:14:01 2021)---
+runfile('C:/Users/tolga/Desktop/TK/PYHTON/bilmece.py', wdir='C:/Users/tolga/Desktop/TK/PYHTON')
+
+## ---(Tue Mar  8 19:37:34 2022)---
+runfile('C:/Users/tolga/Desktop/TK/PYHTON/dolarMatik.py', wdir='C:/Users/tolga/Desktop/TK/PYHTON')
+
+## ---(Tue Mar  8 20:42:00 2022)---
+runfile('C:/Users/tolga/Desktop/TK/PYHTON/bilmece.py', wdir='C:/Users/tolga/Desktop/TK/PYHTON')
+runfile('C:/Users/tolga/Desktop/TK/PYHTON/bigbang.py', wdir='C:/Users/tolga/Desktop/TK/PYHTON')
+5
+runfile('C:/Users/tolga/Desktop/TK/PYHTON/bigbang.py', wdir='C:/Users/tolga/Desktop/TK/PYHTON')
